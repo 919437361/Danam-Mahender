266bf7a3d0a331c4862e12137750c49336c6b779
S516470 <S516470@NW26272.nwmissouri.edu>
2013-06-04 11:27:27 -0500

    i have  added a print statement(Danam mahender)

---
+++ src/mahendergit/Mahendergit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mahendergit;
+
+/**
+ *
+ * @author S516470
+ */
+public class Mahendergit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
